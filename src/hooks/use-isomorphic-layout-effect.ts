024bec5afe4213af3359750e746c4c2cd942e959
arzafran <franco@studiofreight.com>
2022-05-11 16:54:31 -0300

    update isomorpic layout

--- src/hooks/use-isomorphic-layout-effect.ts
+++ src/hooks/use-isomorphic-layout-effect.ts
@@ -1,6 +1,6 @@
 import { useEffect, useLayoutEffect as vanillaUseLayoutEffect } from 'react'
 import { isBrowser } from '../misc/util'
 
-const useLayoutEffect = isBrowser ? vanillaUseLayoutEffect : useEffect
+export const useLayoutEffect = isBrowser ? vanillaUseLayoutEffect : useEffect
 
 export default useLayoutEffect
