13ced9e19ab0490f577ceb83ec8caf5c84c392b8
arzafran <franco@studiofreight.com>
2022-05-19 10:23:19 -0300

    bring back usememo

--- src/hooks/use-debug.js
+++ src/hooks/use-debug.js
@@ -1,9 +1,14 @@
 import { useRouter } from 'next/router'
+import { useMemo } from 'react'
 
 export const useDebug = () => {
   const router = useRouter()
 
-  return router.asPath.includes('#debug') || process.env.NODE_ENV === 'development'
+  const debug = useMemo(() => {
+    router.asPath.includes('#debug') || process.env.NODE_ENV === 'development'
+  }, [router])
+
+  return debug
 }
 
 export default useDebug
