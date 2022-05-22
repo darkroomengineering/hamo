ddf90f70f832e741bf88ac013dc10c7a6588070d
arzafran <franco@studiofreight.com>
2022-05-22 18:14:21 -0300

    minor updates

--- docs/App.jsx
+++ docs/App.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
-import { useDebug, useDocumentReadyState, useIsTouchDevice } from '../src/index'
+import {
+  useDebug,
+  useDocumentReadyState,
+  useIsTouchDevice,
+  useRect,
+} from '../src'
 
 function App() {
+  const [ref, compute] = useRect(1000)
   const isTouch = useIsTouchDevice()
   const debug = useDebug()
   const ready = useDocumentReadyState()
+
+  const rect = compute()
 
   return (
     <main className="main">
       <p>is touch? {isTouch ? 'yes' : 'no'}</p>
       <p>is debug? {debug ? 'yes' : 'no'}</p>
       <p>is document ready? {ready ? 'yes' : 'no'}</p>
+      <div ref={ref}>top: {rect.top}</div>
     </main>
   )
 }
