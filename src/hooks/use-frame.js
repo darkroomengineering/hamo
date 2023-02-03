ac2cc6dd1b12dc36091c053cf312b169eb79d26c
Clément Roche <rchclement@gmail.com>
2023-02-03 20:06:07 +0100

    update tempus

--- src/hooks/use-frame.js
+++ src/hooks/use-frame.js
@@ -2,7 +2,6 @@
  * Creates a requestAnimationFrame loop and manages the lifecycle of the callback.
  * @param {Function} callback - A function that is called every frame.
  * @param {Number} priority - A number that determines the order in which the callback is called.
- * @returns {Number} The ID of the callback.
  */
 import { raf } from '@studio-freight/tempus'
 import { useEffect } from 'react'
@@ -10,10 +9,9 @@ import { useEffect } from 'react'
 export function useFrame(callback, priority = 0) {
   useEffect(() => {
     if (callback) {
-      const id = raf.add(callback, priority)
-      return () => {
-        raf.remove(id)
-      }
+      raf.add(callback, priority)
+
+      return () => raf.remove(callback)
     }
   }, [callback, priority])
 }
